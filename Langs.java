5fb5f7a68e1c89b4891f79d5d2faa4b1557c08a6
Messy Toilet <jacob.chiu168@hotmail.com>
2023-01-21 19:33:46 -0500

    Foreign Languages

---
+++ Langs.java
@@ -0,0 +1,28 @@
+public class Langs {
+
+    public String UniqueRussainCharacters() {
+        String characters = "БбВвГгДдЕеЁёЖжЗзИиЙйКкЛлМмНнПпРрСсТтУуФфХхЦцЧчШшЩщЪъЫыЬьЭэЮюЯя";
+        return characters;
+    }
+
+    public String UniqueChineseCharacters() {
+        String characters = "手田水口廿卜山戈人心日尸木火土竹十大中難金女月弓";
+        return characters;
+    }
+
+    public String UniqueJapaneseCharacters() {
+        String characters = "ぁあぃいぅうぇえぉおかがきぎくぐけげこごさざしじすずせぜそぞただちぢっつづてでとどなにぬねのはばぱひびぴふぶぷへべぺほぼぽまみむめもゃやゅゆょよらりるれろゎわゐゑをんゔゕゖ ゚゛゜ゝゞゟ゠ァアィイゥウェエォオカガキギクグケゲコゴサザシジスズセゼソゾタダチヂッツヅテデトドナニヌネノハバパヒビピフブプヘベペホボポマミムメモャヤュユョヨラリルレロヮワヰヱヲンヴヵヶヷヸヹヺーヽヾヿ";
+        return characters;
+    }
+
+    public String UniqueGreekCharacters() {
+        String characters = "ζχξωβνμλκςηγφδσαθωερτψυιοπώΰϋύόΐϊίήέά€";
+        return characters;
+    }
+
+    public String UniqueIPACharacters() {
+        String characters = "ɑæɐɑ̃βɓʙçɕðd͡ʒɖɗəɚɵɘɛɜɛ̃ɝɞɠʛɢɡħɦɥɧʜɪɨɪ̈ʝɟʄɫɭɬʟɮɱŋɲɳɴɔœɒɔ̃ɶøɸɾɹʁʀɻɽɺʃʂθt͡ʃt͡sʈʊʉʌʋⱱɯʍɰχɣʎʏɤʒʐʑʔʕʡʢ|‖∅˩˨˧˦˥˥˩˩˥˩˨˦˥˧˦˧᷈᷄᷅̌̂ˤˤᵝᵊʱˡⁿʳᵗ";
+        return characters;
+    }
+}
+
